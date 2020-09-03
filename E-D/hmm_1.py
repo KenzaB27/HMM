a15de867a9cc8596ff0838ae23bdf1f9252dd05a
Kenzananas <bouzidkenza1@gmail.com>
2020-09-03 11:38:56 +0200

    alpha pass withouth scaling

--- E-D/hmm_1.py
+++ E-D/hmm_1.py
@@ -1,21 +1,5 @@
 import sys
 
-
-def matrix_multiplication(mat_A, mat_B):
-    result = []
-    for i in range(0, len(mat_A)):
-        # iterate through columns of Y
-        temp = []
-        for j in range(0, len(mat_B[0])):
-            s = 0
-        # iterate through rows of Y
-            for k in range(0, len(mat_A[0])):
-                s += mat_A[i][k] * mat_B[k][j]
-            temp.append(s)
-        result.append(temp)
-    return result
-
-
 matrices = []
 observations = []
 N = 0 
@@ -42,36 +26,22 @@ for line in sys.stdin:
 A = matrices[0]
 B = matrices[1]
 pi = matrices[2][0]
-c0 = 0
 alpha0 = [0] * N
+
 # compute alpha0
 for i in range(N):
     alpha0[i] = pi[i]*B[i][observations[0]]
-    c0 = c0 + alpha0[i]
-print('before scaling ', alpha0)    
-# scale alpha0
-c0 = 1/c0
-for i in range(N):
-    alpha0[i] = c0*alpha0[i]
-print(alpha0)
+
 # compute alphati
 alphat1 = alpha0.copy()
 for t in range(1,T):
-    ct = 0
     alphat = [0] * N
     for i in range(N):
         for j in range(N):
             alphat[i] = alphat[i] + alphat1[j]*A[j][i]
         alphat[i] = alphat[i] * B[i][observations[t]]
-        ct = ct + alphat[i]
-
-    # scale alphat[i]
-    ct = 1/ct
-    for i in range(N):
-        alphat[i] = alphat[i] * ct
 
     alphat1 = alphat.copy()
 
-print(alphat1)
 print(sum(alphat1))
 
